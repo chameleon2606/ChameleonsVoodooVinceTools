3dd2b8bc57c6b9ccac1a9d7a8f79d5b386c6ced3
chameleon2606 <leon@gerstetter.com>
2025-08-05 21:08:06 +0200

    bug fix

--- main_window.h
+++ main_window.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
-static std::string combined_path = "";
-static char game_path_input[128];
+static std::string combined_path;
+static char game_path_input[128] = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Voodoo Vince Remastered\\";
 constexpr char path_extention[128] = "vincedata\\levels\\";
 void init_main();
 void main_loop();
